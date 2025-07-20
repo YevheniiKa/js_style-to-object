9eed81844885c444696fec27f5a9ce2aea3cdd82
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-07-20 20:06:44 +0300

    add solution to the task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const object = {};
+  const styles = sourceString.trim().split(';'); // make array with elements
+
+  // made a loop for for elements of array(styles)
+  for (const element of styles) {
+    const styleValue = element.split(':');
+    // for every element made a array with
+    // first element - name of style and others elements - values of this styles
+    const [style, ...values] = styleValue;
+
+    // add to object style and values without spaces
+    // at the end & start, and with spaces between values
+    object[style.trim()] = values.join(' ').trim();
+
+    // if key is empty - delete this key from object
+    if (!style) {
+      delete object[style];
+    }
+  }
+
+  return object;
 }
 
 module.exports = convertToObject;
