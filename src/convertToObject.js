55be2d6ab1fdc631be0932b3a7c887a2ac5d1871
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-07-22 17:11:33 +0300

    test

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,7 +7,11 @@
  */
 function convertToObject(sourceString) {
   const object = {};
-  const styles = sourceString.trim().split(';'); // make array with elements
+
+  // made array with elements, and filter empty elements
+  const styles = sourceString
+    .split(';')
+    .filter((item) => (typeof item === 'string' ? item.trim() !== '' : true));
 
   // made a loop for for elements of array(styles)
   for (const element of styles) {
@@ -16,17 +20,51 @@ function convertToObject(sourceString) {
     // first element - name of style and others elements - values of this styles
     const [style, ...values] = styleValue;
 
-    // add to object style and values without spaces
-    // at the end & start, and with spaces between values
     object[style.trim()] = values.join(' ').trim();
 
-    // if key is empty - delete this key from object
-    if (!style) {
-      delete object[style];
-    }
+    // add to object style and values without spaces
+    // at the end & start, and with spaces between values
   }
 
   return object;
 }
+
+const complexStylesString = `
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`;
+
+console.log(convertToObject(complexStylesString));
 
 module.exports = convertToObject;
